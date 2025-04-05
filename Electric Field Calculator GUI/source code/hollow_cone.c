acb3adb859bd2b18ab2831a8da4f368f59c861e0
Raza <141234596+KMORaza@users.noreply.github.com>
2025-04-05 13:30:49 +0530

    Delete Electric Field Calculator GUI/source code/hollow_cone.c

--- Electric Field Calculator GUI/source code/hollow_cone.c
+++
@@ -1,9 +0,0 @@
-#include "electric_field.h"
-#include <math.h>
-
-double hollow_cone_field(double sigma, double R, double h, Point3D point) {
-    // Placeholder: Replace with actual field calculation for hollow cone
-    double k = 8.99e9;
-    double area = M_PI * R * sqrt(R * R + h * h);  // Lateral surface area
-    return k * sigma * area / calculate_distance((Point3D){0,0,0}, point);
-}
